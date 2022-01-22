2af36ac34f892f1031d7021ee6ac24b1c7179914
alexey87100 <57604417+alexey87100@users.noreply.github.com>
2022-01-22 23:25:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import datetime as dt
 class Record:
     def __init__(self, amount, comment, date=''):
         self.amount = amount
+        # Не стоит злоупотреблять использованием тернарного оператора
+        # Eсли приходится разносить оператор на несколько строк, страдает его
+        # читаемость. В данной ситуации лучше использовать стандартный блок if-else
         self.date = (
             dt.datetime.now().date() if
             not
@@ -21,6 +24,8 @@ class Calculator:
 
     def get_today_stats(self):
         today_stats = 0
+        # Имя параметра цикла не соответсвует PEP8, более того,
+        # сопадает с именем существующего класса. Следует выбрать другое имя
         for Record in self.records:
             if Record.date == dt.datetime.now().date():
                 today_stats = today_stats + Record.amount
@@ -33,15 +38,20 @@ class Calculator:
             if (
                 (today - record.date).days < 7 and
                 (today - record.date).days >= 0
+            # Нет необходимости в переносе ): на другую строку.
             ):
                 week_stats += record.amount
         return week_stats
 
 
 class CaloriesCalculator(Calculator):
+    # Комментарий к функции не соответствуют требованиям PEP8.
     def get_calories_remained(self):  # Получает остаток калорий на сегодня
         x = self.limit - self.get_today_stats()
         if x > 0:
+            # Стандарт PEP8 не рекомендует использовать \ для переноса
+            # Корректный перенос используется вами ниже в if cash_remained > 0
+            # Следует придерживаться одного стиля во всем документе
             return f'Сегодня можно съесть что-нибудь' \
                    f' ещё, но с общей калорийностью не более {x} кКал'
         else:
@@ -49,6 +59,8 @@ class CaloriesCalculator(Calculator):
 
 
 class CashCalculator(Calculator):
+    # Есть ли смысл использовать преобразование типа? Можно сразу положить
+    # в константы значение типа float (60.0, 70.0)
     USD_RATE = float(60)  # Курс доллар США.
     EURO_RATE = float(70)  # Курс Евро.
 
@@ -63,19 +75,30 @@ class CashCalculator(Calculator):
             cash_remained /= EURO_RATE
             currency_type = 'Euro'
         elif currency_type == 'rub':
+            # По всей видимости опечатка? Следует исправить на /= 1.0
+            # если есть необходимость выводить остаток типа float
             cash_remained == 1.00
             currency_type = 'руб'
+        # Возможно, следует описать обработку ситуации, когда не был передан
+        # параметр currency. Сейчас программа выведет некоторую сумму без
+        # указания единиц измерения
         if cash_remained > 0:
             return (
+                # Согласно условию задания не допускаются вызовы функций в f-строках
                 f'На сегодня осталось {round(cash_remained, 2)} '
                 f'{currency_type}'
             )
         elif cash_remained == 0:
             return 'Денег нет, держись'
         elif cash_remained < 0:
+            # Следует придерживаться одного стиля во всем документе
+            # Выше использовались f строки
             return 'Денег нет, держись:' \
                    ' твой долг - {0:.2f} {1}'.format(-cash_remained,
                                                      currency_type)
-
+    # Нет необходимости в переопределнии функции, она будет унаследована
+    # от класса-родителя. Все что идет ниже нужно убрать
     def get_week_stats(self):
         super().get_week_stats()
+
+# Код не закрыт конструкцией if __name__ == ‘__main__’
